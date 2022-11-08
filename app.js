1a18331c359f585452374d167fdb6da73567020d
Adnan Bakaev <adnanbakaev@MacBook-Air-Adnan.local>
2022-11-08 11:52:25 +0100

    express 3

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-
 const app = express();
+app.use(express.json());
 
-const port = process.env.APP_PORT ?? 5000;
+const userHandlers = require("./userHandlers");
+const movieHandlers = require("./movieHandlers");
+
+const port = process.env.APP_PORT ?? 5070;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -11,12 +14,8 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-
-const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
@@ -28,3 +27,6 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", userHandlers.postNewUser);
